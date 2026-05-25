ed957711f1a67b6ed2c2c746dba11f8c9823e725
Илья Алешин <i.aleshin@ktsstudio.ru>
2026-05-25 10:29:11 +0300

    delete vm from tf

--- infra/main.tf
+++ infra/main.tf
@@ -30,21 +30,6 @@ resource "mws_vpc_egress_nat" "egress_nat" {
   }
 }
 
-data "mws_compute_image" "image" {
-  image   = "mws-ubuntu-2204-lts-v20250529"
-  project = "mws-ubuntu"
-}
-
-resource "mws_vpc_address" "vm_primary_network_interface_address" {
-  network = mws_vpc_network.network.network
-  subnet  = mws_vpc_subnet.subnet.metadata.id
-  address = "vm-address"
-}
-
-resource "mws_vpc_external_address" "vm_external_address" {
-  external_address = "vm-external-address"
-}
-
 resource "mws_vpc_address" "mk8s_primary_endpoint_address" {
   network = mws_vpc_network.network.network
   subnet  = mws_vpc_subnet.subnet.metadata.id
@@ -53,74 +38,6 @@ resource "mws_vpc_address" "mk8s_primary_endpoint_address" {
 
 resource "mws_vpc_external_address" "mk8s_public_endpoint_address" {
   external_address = "${var.mk8s_cluster_name}-public-endpoint"
-}
-
-resource "mws_compute_disk" "disk" {
-  disk      = "mydisk"
-  disk_type = "diskTypes/nbs-pl2"
-  iops      = 1000
-  size      = "10GB"
-
-  source = {
-    image = data.mws_compute_image.image.metadata.id
-  }
-}
-
-resource "mws_compute_virtual_machine" "vm" {
-  virtual_machine = var.vm_name
-  vm_type         = "vmTypes/gen-2-4"
-
-  hardware = {
-    power = "ON"
-  }
-
-  os = {
-    hostname     = "ubuntu"
-    local_domain = "local"
-
-    metadata = {
-      attributes = {
-        user-data = templatefile("./cloud-init.yaml", {})
-      }
-    }
-  }
-
-  storage = {
-    disks = [
-      {
-        name = "boot"
-        boot = true
-        disk = {
-          ref = mws_compute_disk.disk.metadata.id
-        }
-      }
-    ]
-  }
-
-  network = {
-    network_interfaces = [
-      {
-        name    = "primary"
-        primary = true
-
-        addresses = [
-          {
-            address = {
-              ref = mws_vpc_address.vm_primary_network_interface_address.metadata.id
-            }
-
-            one_to_one_nat = {
-              external = {
-                address = {
-                  ref = mws_vpc_external_address.vm_external_address.metadata.id
-                }
-              }
-            }
-          }
-        ]
-      }
-    ]
-  }
 }
 
 resource "mws_vpc_firewall_rule" "firewall_rule" {
@@ -140,13 +57,12 @@ resource "mws_vpc_firewall_rule" "firewall_rule" {
   destination = {
     spec = {
       cidrs = [
-        "${mws_vpc_address.vm_primary_network_interface_address.status.ip_address}/32",
         "${mws_vpc_address.mk8s_primary_endpoint_address.status.ip_address}/32"
       ]
     }
   }
 
-  proto_ports = ["TCP:22", "TCP:80", "TCP:3000", "TCP:6443"]
+  proto_ports = ["TCP:6443"]
   active      = true
 }
 
